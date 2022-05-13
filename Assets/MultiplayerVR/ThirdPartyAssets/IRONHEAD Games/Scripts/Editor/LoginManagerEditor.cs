97c69ed58d3a30f0fea95fbba9bacd162468dd0b
Aleyna Elmas <a.aleynaelmas@gmail.com>
2022-05-13 14:24:26 +0300

    Created login room and added multiple room
    Created login room and login panel
    Home scene connect

---
+++ Assets/MultiplayerVR/ThirdPartyAssets/IRONHEAD Games/Scripts/Editor/LoginManagerEditor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LoginManager))]
+public class LoginManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        EditorGUILayout.HelpBox("This script is responsible for connecting to Photon Servers.", MessageType.Info);
+    }
+}
