832d71a33038a5f6d31825cc4d08cfc278b418f3
Emirhan Aktürk <emirhanakturk@gmail.com>
2022-05-16 00:14:58 +0300

    CubeExplode, CubeKiller, CubePyhsics scripts created

---
+++ Assets/Minigames/SwordAndPistol/Scripts/CubeExplode.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Minigames.SwordAndPistol.Scripts
+{
+    public class CubeExplode : MonoBehaviour
+    {
+
+        public GameObject shatteredObject;
+        public GameObject mainCube;
+        // Start is called before the first frame update
+        void Start()
+        {
+            mainCube.SetActive(true);
+            shatteredObject.SetActive(false);
+        }
+
+        public void IsShot()
+        {
+            Destroy(mainCube);
+            shatteredObject.SetActive(true);
+            var shatterAnimation = shatteredObject.GetComponent<Animation>().Play();
+
+
+            Destroy(shatteredObject,1);
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Bullet")
+            {
+                IsShot();
+            }
+        }
+
+
+    }
+}
