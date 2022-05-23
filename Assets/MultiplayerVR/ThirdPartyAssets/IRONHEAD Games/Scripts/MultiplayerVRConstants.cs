7c813fe45725710972e6d8289fcdca6bdfc48f9c
Aleyna Elmas <a.aleynaelmas@gmail.com>
2022-05-23 17:16:29 +0300

    Remomte avatar model synch

--- Assets/MultiplayerVR/ThirdPartyAssets/IRONHEAD Games/Scripts/MultiplayerVRConstants.cs
+++ Assets/MultiplayerVR/ThirdPartyAssets/IRONHEAD Games/Scripts/MultiplayerVRConstants.cs
@@ -7,5 +7,5 @@ public class MultiplayerVRConstants
     public const string MAP_TYPE_KEY = "map";
     public const string MAP_TYPE_VALUE_SCHOOL = "school";
     public const string MAP_TYPE_VALUE_OUTDOOR = "outdoor";
-    public const int AVATAR_SELECTION_NUMBER = 4;
+    public const string AVATAR_SELECTION_NUMBER = "Avatar_Selection_Number";
 }
