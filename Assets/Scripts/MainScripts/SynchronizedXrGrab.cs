bfd4a51b23cab2df462990595b83913a51977b12
Emirhan Aktürk <emirhanakturk@gmail.com>
2022-05-29 22:31:20 +0300

    simple edits

--- Assets/Scripts/MainScripts/SynchronizedXrGrab.cs
+++ Assets/Scripts/MainScripts/SynchronizedXrGrab.cs
@@ -7,11 +7,28 @@ public class SynchronizedXrGrab : XRGrabInteractable
 {
    private NetworkingGrabbing networkingGrabbing;
 
+   public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+   {
+      base.ProcessInteractable(updatePhase);
+      if (TryGetComponent(out networkingGrabbing))
+      {
+         if (networkingGrabbing.IsBeingHeld)
+         {
+            Debug.Log("1 networkingGrabbing.IsBeingHeld : true");
+            return;
+         }
+      }
+   }
+
    protected override void OnSelectEntered(SelectEnterEventArgs args)
    {
       if (TryGetComponent(out networkingGrabbing))
       {
-         if(networkingGrabbing.IsBeingHeld) return;
+         if (networkingGrabbing.IsBeingHeld)
+         {
+            Debug.Log("2 networkingGrabbing.IsBeingHeld : true");
+            return;
+         }
       }
       
       base.OnSelectEntered(args);
